bee9706d71aa1618b2aa596293cb6e936c6e12bc
suraj1790 <surajkushwaha717@gmail.com>
2025-01-04 20:22:46 +0530

    1st update

--- movie_recommander_system/movie_recommander_app.py
+++ movie_recommander_system/movie_recommander_app.py
@@ -48,31 +48,6 @@ def home():
 @app.route("/recommand",methods=["GET","POST"])
 def recommand():
     return render_template("home.html",top=top_data)
-    # datas = []
-    # # indexes_of_5_movies=None
-    # # movie_index = None
-    # # return render_template("home.html",top = top_data)
-
-    # if request.method == "POST":
-    #     movie_name = request.form.get('movie')
-    #     # return render_template("home.html",top = top_data)
-
-    #     if movie_name not in movies["Title"].values:
-    #         error_msg = "please enter a valid movie name"
-    #         return render_template("error.html", msg=error_msg)
-
-    #     movie_index = movies[movies["Title"] == movie_name].index[0]
-
-    #     distances = similarity[movie_index]
-
-    #     indexes_of_5_movies = sorted(list(enumerate(distances)),reverse=True, key=lambda x:x[1])[1:6]
-
-    #     for i in indexes_of_5_movies:
-    #         datas.append(movies.iloc[i[0],1:-1].values)
-
-    # return render_template("recommand.html",name = datas,top = top_data)
-
-
 
 
 
